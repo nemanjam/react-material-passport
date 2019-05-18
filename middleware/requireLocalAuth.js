bb831eae5d3c1c3ba691db556f3e145d0243fcd6
nemanjam <nemanja.mitic.elfak@hotmail.com>
2019-05-18 20:16:34 +0200

    Custom callback for local strategy.

--- middleware/requireLocalAuth.js
+++ middleware/requireLocalAuth.js
@@ -1,8 +1,21 @@
 const passport = require("passport");
 
-const requireLocalAuth = passport.authenticate("local", {
+const requireLocalAuth1 = passport.authenticate("local", {
   session: false,
   failureFlash: true
 });
+
+const requireLocalAuth = function(req, res, next) {
+  passport.authenticate("local", function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(422).send(info);
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
 
 module.exports = requireLocalAuth;
